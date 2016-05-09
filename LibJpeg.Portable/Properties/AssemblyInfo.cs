8c974d338bdd38a2aefff9d76e2d667a14ae517b
Anders Gustafsson <anders@cureos.com>
2016-05-09 11:48:53 +0200

    Modifications to enable PCL profile 328 build.

---
+++ LibJpeg.Portable/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BitMiracle.LibJpeg.Portable")]
+[assembly: AssemblyDescription("PCL version of IJG's LibJpeg library made by Bit Miracle")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bit Miracle")]
+[assembly: AssemblyProduct("BitMiracle.LibJpeg.Portable")]
+[assembly: AssemblyCopyright("Copyright (C) 2008-2016, Bit Miracle")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: CLSCompliant(true)]
