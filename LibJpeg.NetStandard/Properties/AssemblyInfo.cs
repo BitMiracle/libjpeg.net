4f5c23f31a8b6e9852869887eb590edaac0a43e2
Vitaliy Shibaev <shibvit@gmail.com>
2016-07-18 13:24:11 +0600

    Rename LibJpeg.Portable to LibJpeg.NetStandard

--- LibJpeg.NetStandard/Properties/AssemblyInfo.cs
+++ LibJpeg.NetStandard/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BitMiracle.LibJpeg.Portable")]
-[assembly: AssemblyDescription("PCL version of IJG's LibJpeg library made by Bit Miracle")]
+[assembly: AssemblyDescription(".NET Standard version of IJG's LibJpeg library made by Bit Miracle")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bit Miracle")]
-[assembly: AssemblyProduct("BitMiracle.LibJpeg.Portable")]
+[assembly: AssemblyProduct("BitMiracle.LibJpeg.NetStandard")]
 [assembly: AssemblyCopyright("Copyright (C) 2008-2016, Bit Miracle")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
