99deded5040ba61ba0a47e6dcd94e57e711c20bd
Sergey Bobrovsky <sergius.bobrovsky@gmail.com>
2022-02-10 18:26:00 +0600

    Added initial (no-op) version of JpegTran utility

---
+++ JpegTran/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * This file contains a command-line user interface for JPEG transcoding.
+ * 
+ * It is very similar to cJpeg, and partly to dJpeg, but provides
+ * lossless transcoding between different JPEG file formats.  It also
+ * provides some lossless and sort-of-lossless transformations of JPEG data.
+ */
+
+namespace BitMiracle.JpegTran
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ JpegTran/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JpegTran")]
+[assembly: AssemblyDescription("Lossless JPEG transcoding utility")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bit Miracle")]
+[assembly: AssemblyProduct("Lossless JPEG transcoding utility")]
+[assembly: AssemblyCopyright("Copyright (C) 2022-2022, Bit Miracle")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f40db8ea-4af4-4870-89d8-1bf793c6b1ae")]
